7a3df8a0e585043e0e2d51c15f14d7a505e2471e
vedant-3010 <vedantdandgepro@gmail.com>
2024-02-27 23:32:29 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  let name: string;
+  name = "John Doe";
+
+  let age: number;
+  age = 30;
+
+  let isMarried: boolean;
+  isMarried = false;
+
+  let hobbies: string[];
+  hobbies = ["reading", "coding", "traveling"];
+
+  let address: [string, number];
+
+  let janie: object;
+  
+  
+
+  type Person = {
+    name: string;
+    age?: number;
+    isMarried: boolean;
+    hobbies: string[];
+    address: [string, number];
+  };
+
+  let person: Person;
+
+  person = {
+    name: "John Doe",
+    age: 30,
+    isMarried: false,
+    hobbies: ["reading", "coding", "traveling"],
+    address: ["New York", 10001],
+  };
+
+  let people: Person[];
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React App with typescript</h1>
     </div>
   );
-}
+};
 
 export default App;
